e0445fe5007b5cdc991649eb0b26f40fa2e124d3
JaberKh <j.khaloua@hotmail.com>
2024-08-12 12:47:14 +0200

    Utilisation d'annotation custom et utilisation de PointCutCustom

--- src/main/java/org/example/library/service/BookService.java
+++ src/main/java/org/example/library/service/BookService.java
@@ -1,5 +1,7 @@
 package org.example.library.service;
 
+import org.example.library.annotation.LogAnnotation;
+import org.example.library.annotation.PerformanceAnnotation;
 import org.example.library.model.Book;
 import org.example.library.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,24 +10,33 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+
 @Service
 public class BookService {
 
     @Autowired
     private BookRepository bookRepository;
 
+    @LogAnnotation
+    @PerformanceAnnotation
     public List<Book> getAllBooks() {
         return bookRepository.findAll();
     }
 
+    @LogAnnotation
+    @PerformanceAnnotation
     public Optional<Book> getBookById(Long id) {
         return bookRepository.findById(id);
     }
 
+    @LogAnnotation
+    @PerformanceAnnotation
     public Book createBook(Book book) {
         return bookRepository.save(book);
     }
 
+    @LogAnnotation
+    @PerformanceAnnotation
     public Book updateBook(Long id, Book book) {
         if (bookRepository.existsById(id)) {
             book.setId(id);
@@ -35,7 +46,10 @@ public class BookService {
         }
     }
 
+    @LogAnnotation
+    @PerformanceAnnotation
     public void deleteBook(Long id) {
         bookRepository.deleteById(id);
     }
+
 }
