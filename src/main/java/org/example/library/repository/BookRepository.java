374bf5e103c835d4a291edfc161066db42e6c9d9
JaberKh <j.khaloua@hotmail.com>
2024-08-12 11:22:32 +0200

    Aspects implémentés et test effectués

--- src/main/java/org/example/library/repository/BookRepository.java
+++ src/main/java/org/example/library/repository/BookRepository.java
@@ -1,6 +1,8 @@
 package org.example.library.repository;
 
 import org.example.library.model.Book;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends BaseRepository<Book, Long> {
 }
